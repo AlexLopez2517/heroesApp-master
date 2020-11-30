a0ae068c033e5a71b17eac8fc4fcf08042eb056f
Alex Lopez <alex2761086444@gmail.com>
2020-11-30 11:23:36 -0600

    Obtener un listaedo de todos los héroes

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -33,6 +33,28 @@ export class HeroesService {
     delete heroeTemp.id;
 
   return this.http.put(`${ this.url }/heroes/${heroe.id}.json`, heroe );
-  
     }
+
+    getHeroes() {
+      return this.http.get(`${ this.url }/heroes.json`)
+          .pipe(
+            map( this.crearArrelo )
+          );
+    }
+private crearArrelo( heroesObj: object ){
+
+  const heroes: HeroeModel[] = []
+
+  Object.keys( heroesObj ).forEach( key =>{
+    const heroe: HeroeModel = heroesObj[key];
+    heroe.id = key;
+    heroes.push( heroe );
+  });
+
+  if (heroesObj === null) { return [];}
+
+  return heroes;
+}
+
+
 }
